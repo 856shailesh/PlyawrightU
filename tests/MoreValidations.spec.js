d003644be1531a05de8c4a4e2d22b5b7011dfee9
shaileshkumar <856shailesh@gmail.com>
2025-10-05 07:04:51 +0530

    screenshots section completed

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -17,3 +17,18 @@ test('Popup Validations', async ({ page }) => {
   console.log('End of Script');
   await page.pause();
 });
+
+test('Screenshot & Visual Comparisons', async ({ page }) => {
+  await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+  await expect(page.locator('#displayed-text')).toBeVisible();
+  await page.locator('#displayed-text').screenshot({ path: 'partialSS.png' });
+  await page.locator('#hide-textbox').click();
+  await page.screenshot({ path: 'screenshot.png' });
+  await expect(page.locator('#displayed-text')).toBeHidden();
+});
+
+//Screenshot to screenshot comparison
+test.only('Visual Comparisons', async ({ page }) => {
+  await page.goto('https://google.com/');
+  expect(await page.screenshot()).toMatchSnapshot('landing.png');
+});
